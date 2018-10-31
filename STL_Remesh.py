1b14ff1475760c99e0f0459d9c745837b8ac8388
bfoe <bfoerster@gmail.com>
2018-10-31 16:14:12 -0300

    .

---
+++ STL_Remesh.py
@@ -0,0 +1,138 @@
+#
+# This tool reads an STL mesh file and tries to simplify the mesh 
+#      
+#
+# ----- VERSION HISTORY -----
+#
+# Version 0.1 - 1, November 2018
+#       - 1st public github Release
+#
+# ----- LICENSE -----                 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    For more detail see the GNU General Public License.
+#    <http://www.gnu.org/licenses/>.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#    THE SOFTWARE.
+#
+# ----- REQUIREMENTS ----- 
+#
+#    This program was developed under Python Version 2.7
+#    with the following additional libraries: 
+#    - numpy
+#    - vtk
+#    - PyACVD  ( https://pypi.org/project/PyACVD )
+#
+
+
+from __future__ import print_function
+try: import win32gui, win32console
+except: pass #silent
+import numpy as np
+import sys
+import os
+import warnings
+import vtk
+from PyACVD import Clustering
+
+TK_installed=True
+try: from tkFileDialog import askopenfilename # Python 2
+except: 
+    try: from tkinter.filedialog import askopenfilename; # Python3
+    except: TK_installed=False
+try: import Tkinter as tk; # Python2
+except: 
+    try: import tkinter as tk; # Python3
+    except: TK_installed=False
+if not TK_installed:
+    print ('ERROR: tkinter not installed')
+    print ('       on Linux try "yum install tkinter"')
+    print ('       on MacOS install ActiveTcl from:')
+    print ('       http://www.activestate.com/activetcl/downloads')
+    sys.exit(2)
+
+#general initialization stuff  
+space=' '; slash='/'; 
+if sys.platform=="win32": slash='\\' # not really needed, but looks nicer ;)
+Program_name = os.path.basename(sys.argv[0]); 
+if Program_name.find('.')>0: Program_name = Program_name[:Program_name.find('.')]
+python_version=str(sys.version_info[0])+'.'+str(sys.version_info[1])+'.'+str(sys.version_info[2])
+# sys.platform = [linux2, win32, cygwin, darwin, os2, os2emx, riscos, atheos, freebsd7, freebsd8]
+if sys.platform=="win32": os.system("title "+Program_name)
+    
+#TK initialization       
+TKwindows = tk.Tk(); TKwindows.withdraw() #hiding tkinter window
+TKwindows.update()
+# the following tries to disable showing hidden files/folders under linux
+try: TKwindows.tk.call('tk_getOpenFile', '-foobarz')
+except: pass
+try: TKwindows.tk.call('namespace', 'import', '::tk::dialog::file::')
+except: pass
+try: TKwindows.tk.call('set', '::tk::dialog::file::showHiddenBtn', '1')
+except: pass
+try: TKwindows.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
+except: pass
+TKwindows.update()
+    
+#intercatively choose input STL file
+InputFile = askopenfilename(title="Choose STL Mesh file", filetypes=[("Mesh files",('.stl'))])
+if InputFile=="":print ('ERROR: No input file specified'); sys.exit(2)
+InputFile = os.path.abspath(InputFile)
+TKwindows.update()
+try: win32gui.SetForegroundWindow(win32console.GetConsoleWindow())
+except: pass #silent
+dirname  = os.path.dirname(InputFile)
+basename = os.path.basename(InputFile)
+basename = basename[0:basename.rfind('.stl')]
+    
+    
+# Load mesh from file.
+stlReader = vtk.vtkSTLReader()
+stlReader.SetFileName(InputFile)
+stlReader.Update()
+mesh = stlReader.GetOutput()
+npoints = mesh.GetNumberOfPoints()
+print ('Found %d number of vertices' % npoints)
+
+# Cluster
+decimate = 2.0
+new_npoints = long(npoints/decimate)
+print ('Remeshing with %d vertices' % new_npoints)
+cobj = Clustering.Cluster(mesh)
+cobj.GenClusters(new_npoints, subratio=20, verbose=True)
+warnings.filterwarnings("ignore") 
+cobj.GenMesh()
+
+# Get new mesh
+remesh = cobj.ReturnMesh()
+
+print ('Writing new STL file: '+basename+"_Remesh.stl")
+writer = vtk.vtkSTLWriter()
+writer.SetInputData(remesh)
+writer.SetFileTypeToBinary()
+writer.SetFileName(os.path.join(dirname,basename+'_Remesh.stl'))
+writer.Write()
+print ('done')
+
+if sys.platform=="win32": os.system("pause") # windows
+else: 
+    #os.system('read -s -n 1 -p "Press any key to continue...\n"')
+    import termios
+    print("Press any key to continue...")
+    fd = sys.stdin.fileno()
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    try: result = sys.stdin.read(1)
+    except IOError: pass
+    finally: termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
