0b1fce61ae3ae28d0574035943a5fa2b77cbb9b2
bfoe <bfoerster@gmail.com>
2019-02-28 01:55:51 -0300

    .

--- BrukerOfflineReco_MSME3D.py
+++ BrukerOfflineReco_MSME3D.py
@@ -344,24 +344,22 @@ print('.', end='') #progress indicator
 # (linear phase in time domain = shift in frequency domain)
 #
 #
-AVG = np.average(FIDdata,axis=3)            #complex average all echoes
-PH = FIDdata[:,:,:,:]/AVG[:,:,:,None]       #complex division = phase difference
-PH = np.angle(PH)                           #calc phase angle
-print('.', end='') #progress indicator
-PH = median_filter(PH, size = (5,5,5,1))    #median filter 1
-print('.', end='') #progress indicator
-PH = median_filter(PH, size = (5,5,5,1))    #median filter 2
-print('.', end='') #progress indicator
-s = 7; w = 9; t = (((w - 1)/2)-0.5)/s
-for i in range(dim[3]):
-   PH[:,:,:,i] = gaussian_filter(PH[:,:,:,i], sigma=s, truncate=t)
-print('.', end='') #progress indicator
-FIDdata [:,:,:,:] = np.abs(FIDdata) * np.exp(1j*(np.angle(FIDdata)-PH)) #apply correction
-#save images to check visually
-#aff = np.eye(4)
-#IMG = nib.Nifti1Image(PH, aff)
-#nib.save(IMG, os.path.join(os.path.dirname(FIDfile),OrigFilename+'_testPH.nii.gz'))
-print('.', end='') #progress indicator
+for j in range (3):
+    AVG = np.average(FIDdata[:,:,:,:],axis=3)   #complex average all echoes
+    PH = FIDdata[:,:,:,:]/AVG[:,:,:,None]       #complex division = phase difference
+    PH = np.angle(PH)                           #calc phase angle
+    print('.', end='') #progress indicator    
+    for i in range(dim[3]):    
+       PH[:,:,:,i] = median_filter  (PH[:,:,:,i], size = (5,5,5))    #median filter 1   
+       PH[:,:,:,i] = median_filter  (PH[:,:,:,i], size = (5,5,5))    #median filter 2   
+       PH[:,:,:,i] = gaussian_filter(PH[:,:,:,i], sigma=10, truncate=3)
+       print('.', end='') #progress indicator    
+    FIDdata [:,:,:,:] = np.abs(FIDdata) * np.exp(1j*(np.angle(FIDdata)-PH)) #apply correction
+    #save images to check visually
+    aff = np.eye(4)
+    IMG = nib.Nifti1Image(PH, aff)
+    nib.save(IMG, os.path.join(os.path.dirname(FIDfile),OrigFilename+'_testPH'+str(j+1)+'.nii.gz'))
+    print('.', end='') #progress indicator
 AVG = 0; PH = 0 #free memory
 
 
